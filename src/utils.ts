b938a54214884501ed68d2093cbb4fc5cb46dcee
Kentaro Ozeki <32771324+ozekik@users.noreply.github.com>
2024-08-12 00:00:00 +0900

    fix: replace invalid characters for identifier

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,16 @@ export function interpolate(template: string, params: any) {
   const names = Object.keys(params);
   const vals = Object.values(params);
   // console.log("names, vals", names, vals);
+
+  // Replace invalid characters for identifiers for with underscores in names to avoid errors in `new Function`
+  names.map((name, i) => {
+    names[i] = name
+      .replace(/^[^a-zA-Z_$\p{L}]/u, "_")
+      .replace(/[^\w$\p{L}\p{N}]/gu, "_");
+  });
+
   const result = new Function(...names, `return \`${template}\`;`)(...vals);
   // console.log("result", result);
+
   return result;
 }
